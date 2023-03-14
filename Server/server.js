fe4c7eb25347267a31e5f34148c2af2ab984ac9c
Anubhav-Joria <91547784+Anubhav-Joria@users.noreply.github.com>
2023-03-14 23:17:20 +0530

    /mockApiData added

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,29 @@
 // //HTTP module is used to create a server
-const http = require('http'); // importing module
+const http = require("http"); // importing module
+const fs = require("fs");
+
+const data = fs.readFileSync("../Json/mockApiData.json", "utf-8");
 
 //CREATING HTTP SERVER
-const server = http.createServer((req, resp)=>{
-    //req : for accepting request
-    // resp : for sending response
-    resp.writeHead(200, {"content-type" : "text/html"});// For sending response and we are declaring what type of response we want to send
+const server = http.createServer((req, resp) => {
+  const url = req.url;
+
+  //req : for accepting request
+  // resp : for sending response
+  if (url === "/") {
+    resp.writeHead(200, { "content-type": "text/html" }); // For sending response and we are declaring what type of response we want to send
     resp.write("<h1> First Node app </h1>");
     resp.write("<h2> Creating server and sending response </h2>");
-    resp.end("Ending")
+    resp.end("Ending");
+  }
+  else if (url === "/mockApiData") {
+    resp.writeHead(200, { "content-type": "application/json" }); 
+    resp.end(data);
+  }
+  else{
+    req.statusCode = 404;
+    resp.end("<h1>404 not found</h1>");
+  }
+
 });
-server.listen('9090');
+server.listen("9090");
